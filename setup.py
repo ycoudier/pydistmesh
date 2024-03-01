2cb69204cf4f57d0a277391eaf57234d8e268f16
Yves Coudière <yves.coudiere@u-bordeaux.fr>
2024-03-01 18:08:08 +0100

    Add numpy and scipy as dependencies, and minimal version numbers

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #  Copyright (C) 2012 Bradley Froehle
 
 #  Distributed under the terms of the GNU General Public License. You should
 #  have received a copy of the license along with this program. If not,
 #  see <http://www.gnu.org/licenses/>.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Imports
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 import os
 import numpy as np
@@ -21,9 +21,9 @@ from distutils.extension import Extension
 
 
 # Read version from distmesh/__init__.py
-with open(os.path.join('distmesh', '__init__.py')) as f:
+with open(os.path.join("distmesh", "__init__.py")) as f:
     line = f.readline()
-    while not line.startswith('__version__'):
+    while not line.startswith("__version__"):
         line = f.readline()
 exec(line, globals())
 
@@ -31,47 +31,46 @@ exec(line, globals())
 # Build list of cython extensions
 ext_modules = [
     Extension(
-        'distmesh._distance_functions',
-        sources=[os.path.join('distmesh', '_distance_functions.c')],
-        depends=[os.path.join('distmesh', 'src', 'distance_functions.c')],
+        "distmesh._distance_functions",
+        sources=[os.path.join("distmesh", "_distance_functions.c")],
+        depends=[os.path.join("distmesh", "src", "distance_functions.c")],
         include_dirs=[np.get_include()],
     ),
 ]
 
 # distmesh._distance_functions needs LAPACK
-lapack_info = system_info.get_info('lapack_opt', 0)
+lapack_info = system_info.get_info("lapack_opt", 0)
 # See ('https://github.com/nipy/nipy/blob/'
 #      '91fddffbae25a5ca3a5b35db2a7c605b8db9014d/nipy/labs/setup.py#L44')
-if 'libraries' not in lapack_info:
-    lapack_info = system_info.get_info('lapack', 0)
+if "libraries" not in lapack_info:
+    lapack_info = system_info.get_info("lapack", 0)
 
-ext_modules[0].libraries.extend(lapack_info['libraries'])
-ext_modules[0].library_dirs.extend(lapack_info['library_dirs'])
-if 'include_dirs' in lapack_info:
-    ext_modules[0].include_dirs.extend(lapack_info['include_dirs'])
+ext_modules[0].libraries.extend(lapack_info["libraries"])
+ext_modules[0].library_dirs.extend(lapack_info["library_dirs"])
+if "include_dirs" in lapack_info:
+    ext_modules[0].include_dirs.extend(lapack_info["include_dirs"])
 
-install_requires = [
-    'matplotlib>=1.2',
-]
+install_requires = ["numpy>=1.21.5", "scipy>=1.8.0", "matplotlib>=3.5.1"]
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
 
-setup(name='PyDistMesh',
-      version=__version__,
-      description="A Simple Mesh Generator in Python",
-      long_description=long_description,
-      classifiers=[
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2',
-          'Operating System :: POSIX :: Linux',
-          'Topic :: Scientific/Engineering :: Mathematics',
-      ],
-      keywords='meshing',
-      author='Bradley Froehle',
-      author_email='brad.froehle@gmail.com',
-      url='https://github.com/bfroehle/pydistmesh',
-      install_requires=install_requires,
-      license='GPL',
-      packages=['distmesh'],
-      ext_modules=ext_modules,
+setup(
+    name="PyDistMesh",
+    version=__version__,
+    description="A Simple Mesh Generator in Python",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    keywords="meshing",
+    author="Bradley Froehle",
+    author_email="brad.froehle@gmail.com",
+    url="https://github.com/ycoudier/pydistmesh",
+    install_requires=install_requires,
+    license="GPL",
+    packages=["distmesh"],
+    ext_modules=ext_modules,
 )
