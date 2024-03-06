60d0f9442d65621ebac30ee7fac4fb4116faf5f3
Yves Coudière <yves.coudiere@u-bordeaux.fr>
2024-03-06 16:53:43 +0100

    Replace distutils by setuptools
    It remains numpy.distutils to change, which mean using a meson build

--- setup.py
+++ setup.py
@@ -8,35 +8,10 @@
 #  see <http://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
 
-# -----------------------------------------------------------------------------
-# Imports
-# -----------------------------------------------------------------------------
-
-import os
+from setuptools import Extension, setup
+from pathlib import Path
 import numpy as np
 from numpy.distutils import system_info
-
-from distutils.core import setup
-from distutils.extension import Extension
-
-
-# Read version from distmesh/__init__.py
-with open(os.path.join("distmesh", "__init__.py")) as f:
-    line = f.readline()
-    while not line.startswith("__version__"):
-        line = f.readline()
-exec(line, globals())
-
-
-# Build list of cython extensions
-ext_modules = [
-    Extension(
-        "distmesh._distance_functions",
-        sources=[os.path.join("distmesh", "_distance_functions.c")],
-        depends=[os.path.join("distmesh", "src", "distance_functions.c")],
-        include_dirs=[np.get_include()],
-    ),
-]
 
 # distmesh._distance_functions needs LAPACK
 lapack_info = system_info.get_info("lapack_opt", 0)
@@ -45,32 +20,26 @@ lapack_info = system_info.get_info("lapack_opt", 0)
 if "libraries" not in lapack_info:
     lapack_info = system_info.get_info("lapack", 0)
 
-ext_modules[0].libraries.extend(lapack_info["libraries"])
-ext_modules[0].library_dirs.extend(lapack_info["library_dirs"])
+# ext_modules[0].libraries.extend(lapack_info["libraries"])
+# ext_modules[0].library_dirs.extend(lapack_info["library_dirs"])
 if "include_dirs" in lapack_info:
-    ext_modules[0].include_dirs.extend(lapack_info["include_dirs"])
+    lapack_inc = lapack_info["include_dirs"]
+else:
+    lapack_inc = []
 
-install_requires = ["numpy>=1.21.5", "scipy>=1.8.0", "matplotlib>=3.5.1"]
+print("Lapack libraries are : {}".format(lapack_info["libraries"]))
+print("Lapack library dirs are : {}".format(lapack_info["library_dirs"]))
 
-long_description = open("README.rst").read()
-
+# Build list of cython extensions
 setup(
-    name="PyDistMesh",
-    version=__version__,
-    description="A Simple Mesh Generator in Python",
-    long_description=long_description,
-    classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.2",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-    keywords="meshing",
-    author="Bradley Froehle",
-    author_email="brad.froehle@gmail.com",
-    url="https://github.com/ycoudier/pydistmesh",
-    install_requires=install_requires,
-    license="GPL",
-    packages=["distmesh"],
-    ext_modules=ext_modules,
+    ext_modules=[
+        Extension(
+            name="distmesh._distance_functions",
+            sources=[str(Path("distmesh", "_distance_functions.pyx"))],
+            depends=[Path("distmesh", "src", "distance_functions.c")],
+            include_dirs=[np.get_include()] + lapack_inc,
+            libraries=lapack_info["libraries"],
+            library_dirs=lapack_info["library_dirs"],
+        ),
+    ]
 )
